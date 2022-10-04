d5037af6a2998fe280a55f8512c692e5e2a74498
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-10-04 19:51:32 -0300

    refatoração e requisito 5 concluido

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
+window.onload = function () {
+
+    if (localStorage.getItem('colorPalette') === null) {
+        storageColors(listaCores);
+        for (let i = 0; i < cores.length; i += 1) {
+            let cor = cores[i]
+            cor.style.backgroundColor = listaCores[i]
+        }
+    } else {
+        let novaLista = JSON.parse(localStorage.colorPalette)
+        for (let i = 0; i < cores.length; i += 1) {
+            const cor = cores[i];
+            if (i === 0) {
+                cor.style.backgroundColor = listaCores[i]
+            }
+            cor.style.backgroundColor = novaLista[i - 1];
+        }
+
+    }
+}
+
 const btnColor = document.getElementById('button-random-color');
 const cores = document.getElementsByClassName('color');
 const body = document.getElementsByTagName('body')[0]
+const listaCores = ["rgb(0, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 0, 255)"]
+
+function storageColors(lista) {
+    localStorage.setItem('colorPalette', JSON.stringify(lista));
+}
 
 // requisito 4
 function geraCores() {
-    btnColor.addEventListener('click', function () {
-        for (let i = 1; i < cores.length; i += 1) {
-            let corAleatoria = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
-            let corAtual = cores[i]
-            corAtual.style.backgroundColor = corAleatoria;
-        }
-    })
+    let coresGeradas = []
+    for (let i = 1; i < cores.length; i += 1) {
+        let corAleatoria = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+        let corAtual = cores[i]
+        coresGeradas.push(corAleatoria);
+        corAtual.style.backgroundColor = corAleatoria;
+    }
+    storageColors(coresGeradas);
 }
-geraCores();
 
-
-// requisito 5
+btnColor.addEventListener('click', geraCores)
 
 
 // requisito 6 e 7
@@ -36,15 +61,15 @@ geraQuadro();
 
 // requisito 9
 
-function selecionaCor() {
-    for (let i = 0; i < cores.length; i += 1) {
-        const cor = cores[i]
-        cor.addEventListener('click', function (event) {
-            const selected = document.querySelector('.selected');
-            selected.classList.remove('selected');
-            event.target.classList.add('selected');
-        })
-    }
+function selecionaCor(event) {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');
 }
 
-selecionaCor();
+for (let i = 0; i < cores.length; i += 1) {
+    const cor = cores[i]
+    cor.addEventListener('click', selecionaCor)
+}
+
+// requisito 10
