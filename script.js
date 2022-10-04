26c3cf3046d57441ec88ee415b81a24a726b4cdf
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-10-04 17:10:16 -0300

    requisito 6 e 7 concluido

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+window.onload = function () {
+
+}
+
 const btnColor = document.getElementById('button-random-color');
 const cores = document.getElementsByClassName('color');
+const body = document.getElementsByTagName('body')[0]
 
 // requisito 4
 function geraCores() {
     btnColor.addEventListener('click', function () {
         for (let i = 1; i < cores.length; i += 1) {
             let corAleatoria = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
-            
             let corAtual = cores[i]
             corAtual.style.backgroundColor = corAleatoria;
         }
     })
 }
-
 geraCores();
+
+// requisito 5
+for (let i = 1; i < cores.length; i += 1) {
+    const cor = cores[i]
+    cor.addEventListener('click', function (event) {
+        console.log(event.target.style);
+    })
+}
+
+// requisito 6
+
+function geraQuadro() {
+    const board = document.createElement('section');
+    board.id = 'pixel-board';
+    let numPixels = 25;
+
+    for (let i = 0; i < numPixels; i += 1) {
+        const pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        board.appendChild(pixel);
+    }
+    body.appendChild(board);
+}
+
+geraQuadro();
