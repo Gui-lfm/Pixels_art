3dc3b53af66c2dc7c82e4088ac9066af5cd74b66
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-10-04 20:34:15 -0300

    requisito 10 concluido

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 window.onload = function () {
 
     if (localStorage.getItem('colorPalette') === null) {
-        storageColors(listaCores);
-        for (let i = 0; i < cores.length; i += 1) {
-            let cor = cores[i]
-            cor.style.backgroundColor = listaCores[i]
+        storageColors(colorList);
+        for (let i = 0; i < colors.length; i += 1) {
+            let cor = colors[i]
+            cor.style.backgroundColor = colorList[i]
         }
     } else {
-        let novaLista = JSON.parse(localStorage.colorPalette)
-        for (let i = 0; i < cores.length; i += 1) {
-            const cor = cores[i];
+        let newList = JSON.parse(localStorage.colorPalette)
+        for (let i = 0; i < colors.length; i += 1) {
+            const color = colors[i];
             if (i === 0) {
-                cor.style.backgroundColor = listaCores[i]
+                color.style.backgroundColor = colorList[i]
             }
-            cor.style.backgroundColor = novaLista[i - 1];
+            color.style.backgroundColor = newList[i - 1];
         }
-
     }
 }
 
+// variáveis locais
 const btnColor = document.getElementById('button-random-color');
-const cores = document.getElementsByClassName('color');
+const colors = document.getElementsByClassName('color');
 const body = document.getElementsByTagName('body')[0]
-const listaCores = ["rgb(0, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 0, 255)"]
+const colorList = ['rgb(0, 0, 0)', 'rgb(255, 0, 0)', 'rgb(255, 255, 0)', 'rgb(0, 0, 255)'];
+let brush = colorList[0];
 
-function storageColors(lista) {
-    localStorage.setItem('colorPalette', JSON.stringify(lista));
+function storageColors(list) {
+    localStorage.setItem('colorPalette', JSON.stringify(list));
 }
 
 // requisito 4
 function geraCores() {
     let coresGeradas = []
-    for (let i = 1; i < cores.length; i += 1) {
+    for (let i = 1; i < colors.length; i += 1) {
         let corAleatoria = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
-        let corAtual = cores[i]
+        let corAtual = colors[i]
         coresGeradas.push(corAleatoria);
         corAtual.style.backgroundColor = corAleatoria;
     }
@@ -42,9 +43,8 @@ function geraCores() {
 
 btnColor.addEventListener('click', geraCores)
 
-
 // requisito 6 e 7
-function geraQuadro() {
+function createBoard() {
     const board = document.createElement('section');
     board.id = 'pixel-board';
     let numPixels = 25;
@@ -57,19 +57,29 @@ function geraQuadro() {
     body.appendChild(board);
 }
 
-geraQuadro();
+createBoard();
+const pixels = document.getElementsByClassName('pixel');
 
 // requisito 9
-
-function selecionaCor(event) {
+function selectColor(event) {
     const selected = document.querySelector('.selected');
     selected.classList.remove('selected');
     event.target.classList.add('selected');
+    brush = document.querySelector('.selected').style.backgroundColor;
 }
 
-for (let i = 0; i < cores.length; i += 1) {
-    const cor = cores[i]
-    cor.addEventListener('click', selecionaCor)
+for (let i = 0; i < colors.length; i += 1) {
+    const color = colors[i]
+    color.addEventListener('click', selectColor)
 }
 
 // requisito 10
+function paintPixels(event) {
+    let pixelSelecionado = event.target;
+    pixelSelecionado.style.backgroundColor = brush;
+}
+
+for (let i = 0; i < pixels.length; i += 1) {
+    const pixel = pixels[i]
+    pixel.addEventListener('click', paintPixels)
+}
